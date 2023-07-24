a055d479edc48ce07202ce4df23c86c6074381db
bhumika-kedia <140391691+bhumika-kedia@users.noreply.github.com>
2023-07-24 15:40:38 +0530

    Update strealit_app.py
    header changes
    added menu pictures

--- strealit_app.py
+++ strealit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 3  & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('HArd-boiled Free Range Egg')
+streamlit.text(' 🥣 Omega 3  & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
